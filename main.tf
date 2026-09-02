fb22c83e1c6fec82df9bdb88585acaac3c779561
mohdouaakha-dot <moh.douaakha@gmail.com>
2026-09-02 08:04:05 +0000

    Add Terraform code and CI/CD workflow

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-cicd-lab"
+  location = "East US"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "asp-terraform-lab"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "app-tf-lab-${random_integer.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.plan.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    always_on = false
+    application_stack {
+      node_version = "18-lts"
+    }
+  }
+}
+
+resource "random_integer" "suffix" {
+  min = 10000
+  max = 99999
+}
